70e725379ed5b978863c9aa510af2b9dd8747a11
Andre Eka Putra Simanjuntak <Andremoore431@gmail.com>
2023-10-09 04:00:35 +0700

    Add Space

--- app/src/main/java/com/andre/cinamate/MainActivity.kt
+++ app/src/main/java/com/andre/cinamate/MainActivity.kt
@@ -33,6 +33,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         setContentView(binding.root)
+
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment
 
